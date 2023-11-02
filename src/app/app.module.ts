b98e0a3cf1e3abb2351cc453c29e267789805b88
Lucas <lucas.y.tan.life@outlook.com>
2023-11-02 22:41:32 +0800

    update kla stack and apm moniotr setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApmService } from '@elastic/apm-rum-angular';
+import { ErrorHandler } from '@angular/core'
+import { ApmErrorHandler } from '@elastic/apm-rum-angular'
 
 @NgModule({
   declarations: [
@@ -12,7 +15,24 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ApmService,
+    {
+      provide: ErrorHandler,
+      useClass: ApmErrorHandler
+    }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(service: ApmService) {
+    // Agent API is exposed through this apm instance
+    const apm = service.init({
+      serviceName: 'angular-app',
+      serverUrl: 'http://localhost:8200'
+    })
+
+    apm.setUserContext({
+      'username': 'foo',
+      'id': 'bar'
+    })
+  }
+}
